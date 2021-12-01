14bc05471bd9d3326b27f1af2596939a756613d6
chehuiw <chehuiw@hotmail.com>
2021-12-01 17:36:33 +0100

    handle overlap

--- haplotype/naivehap.py
+++ haplotype/naivehap.py
@@ -48,6 +48,8 @@ def index_frag_mc(inbam):
             if (read1ref == read2ref):
                 read1start = read1.reference_start + 1
                 read2start = read2.reference_start + 1
+                read1end = read1.reference_end
+                read2end = read2.reference_end
                 ftlen = abs(read1.template_length)
                 #pysam reference_start: 0-based; match bam file to 1-based
                 fleftmost = min(read1start, read2start)
@@ -58,27 +60,59 @@ def index_frag_mc(inbam):
                 read2xm = read2.get_tag('XM')
                 frag_dict = defaultdict(int)
 
+                #evalute letfmost read
                 if fleftmost == read1start:
+                    lreadstart = read1start
+                    matestart = read2start
                     fragstartXM = read1xm
                     fragmergeXM = read2xm
 
                 else:
+                    lreadstart = read2start
+                    matestart = read1start
                     fragstartXM = read2xm
                     fragmergeXM = read1xm
 
+                #evalute righmost read
+                if frightmost == read1end:
+                    lreadend = read2end
+                    mateend = read1end
+                else:
+                    lreadend = read1end
+                    mateend = read2end
+
                 #evaluate overlap
-                if (read1.reference_end < read2start):
+                if (read1end < read2start):
                     ##use N to fill gap between R1 and R2
                     ##to do check softclip
-                    read1end = read1.reference_end
-                    print(read1end)
                     fillgapN = "N"*(read2start - read1end - 1)
                     fxmstring = f'{fragstartXM}{fillgapN}{fragmergeXM}'
                     pfline = f'{readname}\t{read1ref}\t{fleftmost}\t{frightmost}\t{fxmstring}'
                     print(pfline)
+                elif (read1start <= read2start and read1end >= read2end) or (read2start <= read1start and read2end >= read1end):
+                    #full overlap; one read covers the mate entirely
+                    #code unmatched mC call from R1 and R2 as 'D'
+                    overlapxm = ""
+                    overlapSpos = matestart - lreadstart
+                    overlapEpos = lreadend - matestart + overlapSpos + 1
+                    leftxmstring = f'{fragstartXM[0:(overlapSpos)]}'
+                    rightxmstring = f'{fragstartXM[overlapEpos:]}'
+                    for i in range(overlapSpos,overlapEpos):
+                        if (fragstartXM[i] == fragmergeXM[i-overlapSpos]):
+                            overlapxm += fragstartXM[i]
+                        else:
+                            overlapxm += 'D'
+                    fxmstring = f'{leftxmstring}{overlapxm}{rightxmstring}'
+                    pfline = f'{readname}\t{read1ref}\t{fleftmost}\t{frightmost}\t{fxmstring}'
+                    print(pfline)
+
                 else:
-                    continue
-                    #merge overlap here
+                    #partial overlap; merge overlap here
+                    overlapSpos = matestart - lreadstart
+                    overlapEpos = lreadend - matestart + overlapSpos + 1
+                    leftxmstring = f'{fragstartXM[0:(overlapSpos)]}'
+                    rightxmstring = f'{fragmergeXM[overlapEpos:]}'
+
 
 if __name__ == '__main__':
     index_frag_mc(sys.argv[1])
