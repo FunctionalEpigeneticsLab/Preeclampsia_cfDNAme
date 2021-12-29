a1361e7f831ab5a2d62422c7d6392fe75abba32c
chehuiw <chehuiw@hotmail.com>
2021-12-29 19:06:23 +0100

    construct

---
+++ upstream/helper.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import configparser
+
+curdir = os.getcwd()
+
+def mkdir(sdir):
+    if not os.path.isdir(sdir):
+        os.makedirs(sdir)
+
+def check_fexist(f):
+    if os.path.isfile(f) == True:
+        return(1)
+    else:
+        return(0)
+
+def check_dirslash(d):
+    if d.strip()[-1] == '/':
+        return(d)
+    else:
+        d = d + '/'
+        return(d)
+    print(d)
+
+def file_remove(fh):
+    try:
+        os.remove(fh)
+    except OSError:
+        raise
+
+def get_dir_config():
+    parser = configparser.ConfigParser()
+    ConfigFile = curdir + '/ConfigFile'
+    parser.read('ConfigFile')
+
+    fastqdir = parser.get('configDir', 'fastqdir')
+    workdir = parser.get('configDir', 'workdir')
+
+    return(fastqdir, workdir)
+
+def get_run_config():
+    parser = configparser.ConfigParser()
+    ConfigFile = curdir + '/ConfigFile'
+    parser.read('ConfigFile')
+
+    samtools = parser.get('configTool', 'samtools')
+    samtoolsdir = parser.get('configTool', 'samtoolsdir')
+    fastqc = parser.get('configTool', 'fastqc')
+    trimtool = parser.get('configTool', 'trimtool')
+    bismark = parser.get('configTool', 'bismark')
+    bismarkdedup = parser.get('configTool', 'bismarkdedup')
+    bismarkextractor = parser.get('configTool', 'bismarkextractor')
+    bismarkcytosine = parser.get('configTool', 'bismarkcytosine')
+    java = parser.get('configTool', 'java')
+    gatk = parser.get('configTool', 'gatk')
+    picard = parser.get('configTool', 'picard')
+    addsourcetool = parser.get('configTool', 'addsourcetool')
+
+    bismarkreference = parser.get('configFile', 'bismarkreference')
+    reference = parser.get('configFile', 'reference')
+    target = parser.get('configFile', 'target')
+    targetflank50 = parser.get('configFile', 'targetflank50')
+    targetflank100 = parser.get('configFile', 'targetflank100')
+
+    return(samtools, samtoolsdir, fastqc, trimtool, bismark, bismarkdedup, bismarkextractor, bismarkcytosine, java, gatk, picard, addsourcetool, bismarkreference, reference, target, targetflank50, targetflank100)
+
+def get_DNA_sample():
+    parser = configparser.ConfigParser()
+    ConfigFile = curdir + '/ConfigFile'
+    parser.read('ConfigFile')
+
+    DNAsamplelist = parser.get('configFile', 'samplelist')
+
+    DNAsampleID = []
+
+    with open(DNAsamplelist, 'r') as fh0:
+        for l0 in fh0:
+            DNAsampleID.append(l0.rstrip('\n'))
+    fh0.close()
+
+    return(DNAsampleID)
+
+def get_DNAproc_config():
+    parser = configparser.ConfigParser()
+    ConfigFile = curdir + '/ConfigFile'
+    parser.read('ConfigFile')
+
+    runscriptdir = parser.get('configDir', 'runscriptdir')
+    Rscript = parser.get('configTool', 'Rscript')
+    return(runscriptdir, Rscript)
+
+def get_option():
+    parser = configparser.RawConfigParser()
+    ConfigFile = curdir + '/ConfigFile'
+    parser.read('ConfigFile')
+
+    DNAtrimoption = parser.get('configParam','DNAtrimoption')
+
+    return(DNAtrimoption)
+
+def get_userinfo():
+    parser = configparser.RawConfigParser()
+    ConfigFile = curdir + '/ConfigFile'
+    parser.read('ConfigFile')
+
+    noticeaccount = parser.get('configUser','noticeaccount')
+    creditaccount = parser.get('configUser','creditaccount')
+
+    return(noticeaccount, creditaccount)
---
+++ upstream/process.cfDNA.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import time
+import helper
+
+(fastqdir, workdir) = helper.get_dir_config()
+(samtools, samtoolsdir, fastqc, trimtool, bismark, bismarkdedup, bismarkextractor, bismarkcytosine, java, gatk, picard, addsourcetool, bismarkreference, reference, target, targetflank50, targetflank100) = helper.get_run_config()
+DNASampleIDs = helper.get_DNA_sample()
+(DNAtrimoption) = helper.get_option()
+(noticeaccount, creditaccount) = helper.get_userinfo()
+
+def write_sub_script_qsub(taskscriptdir, taskname, taskworkdir, stderrdir):
+    '''
+    mpinum added in case of parallel jobs needed
+    taskname added in case of multiple qsub in one scriptdir
+    stderrdir added in case output to terminal or stdout
+    '''
+    qsub = 'qsub'
+
+    task_qsub = taskscriptdir + '/' + taskname + '.qsub.sh'
+    with open(task_qsub, 'w+') as fo:
+        fo.write('#!/bin/bash -l\n\n')
+
+        for script in os.listdir(taskscriptdir):
+            tasksearch = taskname + '.task.pbs'
+            if script.endswith(tasksearch):
+                #myqsub = '%s -d %s -e %s -o %s %s\n' % (qsub, taskworkdir, stderrdir, logdir, script)
+                myqsub = '%s %s\n' % (qsub, script)
+                fo.write(myqsub)
+    return(task_qsub)
+
+
+def write_sub_script_fastqc(SampleIDs, fastqdir, scriptdir, workdir, logdir):
+    fastqc_scriptdir = scriptdir + '/1_fastqc'
+    fastqc_workdir = workdir + '/1_qc'
+    helper.mkdir(fastqc_scriptdir)
+    helper.mkdir(fastqc_workdir)
+
+    for sampleid in SampleIDs:
+
+        samplescript = fastqc_scriptdir + '/' + sampleid + '.fastqc.task.pbs'
+
+        with open(samplescript, 'w+') as fo:
+            pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (1)
+            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (2)
+            pbsuser = '#PBS -M %s\n' % (noticeaccount)
+            pbsjobname = '#PBS -N fastqc_%s\n' % (sampleid)
+            pbscredit = '#PBS -A %s\n\n' % (creditaccount)
+            pbsaddsource = 'source %s\n\n' % (addsourcetool)
+            fo.write('#!/bin/bash -l\n\n')
+            fo.write(pbsjobtime)
+            fo.write(pbsnode)
+            fo.write('#PBS -l pmem=5gb\n')
+            fo.write(pbsuser)
+            fo.write('#PBS -m abe\n')
+            fo.write(pbsjobname)
+            fo.write(pbscredit)
+            fo.write(pbsaddsource)
+
+            for fastqfile in os.listdir(fastqdir):
+                if fastqfile.startswith(sampleid) and (fastqfile.endswith('R1.fastq.gz') or fastqfile.endswith('R2.fastq.gz')):
+                    qc_task = '%s -t 2 %s/%s -o %s\n' % (fastqc, fastqdir, fastqfile, fastqc_workdir)
+                    fo.write(qc_task)
+
+    write_sub_script_qsub(fastqc_scriptdir, 'fastqc', fastqc_workdir, logdir)
+
+def write_sub_script_DNAtrimming(DNASampleIDs, fastqdir, scriptdir, workdir, logdir, DNAtrimoption):
+    trim_scriptdir = scriptdir + '/2_DNAtrim'
+    trim_workdir = workdir + '/2_DNAtrim'
+    helper.mkdir(trim_scriptdir)
+    helper.mkdir(trim_workdir)
+
+    for sampleid in DNASampleIDs:
+        samplescript = trim_scriptdir + '/' + sampleid + '.DNAtrim.task.pbs'
+        with open(samplescript, 'w') as fo:
+            pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (1)
+            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (2)
+            pbsuser = '#PBS -M %s\n' % (noticeaccount)
+            pbsjobname = '#PBS -N trim_%s\n' % (sampleid)
+            pbscredit = '#PBS -A %s\n\n' % (creditaccount)
+            #when load home source, jave heap size problem occur; possibly corruption of tool environment; update all tools later
+            #pbsaddsource = 'source %s\n\n' % (addsourcetool)
+            fo.write('#!/bin/bash -l\n\n')
+            fo.write(pbsjobtime)
+            fo.write(pbsnode)
+            fo.write('#PBS -l pmem=5gb\n')
+            fo.write(pbsuser)
+            fo.write('#PBS -m abe\n')
+            fo.write(pbsjobname)
+            fo.write(pbscredit)
+            #fo.write(pbsaddsource)
+
+            for fastqfile in os.listdir(fastqdir):
+                if fastqfile.startswith(sampleid) and fastqfile.endswith('R1.fastq.gz'):
+                    curreadR2 = fastqfile.replace("R1","R2")
+                    readpairs = '%s/%s %s/%s' % (fastqdir, fastqfile, fastqdir, curreadR2)
+                    baseparam = '--length 36'
+                    if (DNAtrimoption == "default"):
+                        addparam = baseparam
+                    else:
+                        addparam = '%s %s' % (baseparam, DNAtrimoption)
+                    #trim_task = '%s --length 36 --clip_R1 9 --clip_R2 9 --three_prime_clip_R1 2 -o %s --paired %s --fastqc\n\n' % (trimtool, trim_workdir, readpairs)
+                    trim_task = '%s %s -o %s --paired %s --fastqc\n\n' % (trimtool, addparam, trim_workdir, readpairs)
+                    fo.write(trim_task)
+
+    write_sub_script_qsub(trim_scriptdir, 'DNAtrim', trim_workdir, logdir)
+
+
+def write_sub_script_DNAalign(DNASampleIDs, fastqdir, scriptdir, workdir, logdir):
+    trim_workdir = workdir + '/2_DNAtrim'
+    DNAalign_scriptdir = scriptdir + '/3_Bismarkalign'
+    DNAalign_workdir = workdir + '/3_Bismark'
+    helper.mkdir(DNAalign_scriptdir)
+    helper.mkdir(DNAalign_workdir)
+
+    for sampleid in DNASampleIDs:
+        samplescript = DNAalign_scriptdir + '/' + sampleid + '.bismark.DNAalign.task.pbs'
+        sample_sub_workdir = DNAalign_workdir + '/' + sampleid
+        helper.mkdir(sample_sub_workdir)
+        lanefastqR1 = []
+        lanefastqR2 = []
+
+        with open(samplescript, 'w') as fo:
+            pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (12)
+            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (6)
+            pbsuser = '#PBS -M %s\n' % (noticeaccount)
+            pbsjobname = '#PBS -N DNAalign_%s\n' % (sampleid)
+            pbscredit = '#PBS -A %s\n\n' % (creditaccount)
+            pbsaddsource = 'source %s\n\n' % (addsourcetool)
+            fo.write('#!/bin/bash -l\n\n')
+            fo.write(pbsjobtime)
+            fo.write(pbsnode)
+            fo.write('#PBS -l pmem=5gb\n')
+            fo.write(pbsuser)
+            fo.write('#PBS -m abe\n')
+            fo.write(pbsjobname)
+            fo.write(pbscredit)
+            fo.write(pbsaddsource)
+
+            for fastqfile in os.listdir(fastqdir):
+                if fastqfile.startswith(sampleid) and fastqfile.endswith('R1.fastq.gz'):
+                #if fastqfile.startswith(sampleid) and fastqfile.endswith('R1_val_1.fq.gz'):
+                    curfastqR1 = '%s/%s' % (fastqdir, fastqfile)
+                    lanefastqR1.append(curfastqR1)
+
+            FQR1 = ','.join(lanefastqR1)
+            trimFQR1 = FQR1.replace(fastqdir, trim_workdir).replace("R1.fastq.gz","R1_val_1.fq.gz")
+            trimFQR2 = trimFQR1.replace("R1_val_1.fq.gz","R2_val_2.fq.gz")
+            #need samtools to get correct file handle
+            #directional library
+            #For cfDNA, allow soft clipping
+            DNAalign_task = '%s --local -p 6 --genome %s --output_dir %s --temp_dir %s --samtools_path %s --gzip -1 %s -2 %s\n\n' % (bismark, bismarkreference, sample_sub_workdir, sample_sub_workdir, samtoolsdir, trimFQR1, trimFQR2)
+            fo.write(DNAalign_task)
+
+            #get output name in another way; this replacement is stupid
+            #alignbams = FQR1.replace(fastqdir, sample_sub_workdir).replace(".fastq.gz","_bismark_bt2_pe.bam").replace(",", " ")
+            alignbams = trimFQR1.replace(trim_workdir, sample_sub_workdir).replace(".fq.gz","_bismark_bt2_pe.bam").replace(",", " ")
+            #check number of lanes
+            if (',' in trimFQR1):
+                dedup_task = '%s -p -o %s --output_dir %s --multiple %s\n\n' % (bismarkdedup, sampleid, sample_sub_workdir, alignbams)
+                dedupbam = '%s/%s.multiple.deduplicated.bam' % (sample_sub_workdir, sampleid)
+            else:
+                dedup_task = '%s -p -o %s --output_dir %s %s\n\n' % (bismarkdedup, sampleid, sample_sub_workdir, alignbams)
+                dedupbam = '%s/%s.deduplicated.bam' % (sample_sub_workdir, sampleid)
+            fo.write(dedup_task)
+
+            filterbam = '%s/%s.merge.deduplicated.filtered.FR.bam' % (sample_sub_workdir, sampleid)
+            #filter out unmapped reads (4), secondary alignment(256), supplementary alignment(2048), mapping quality < 40 -- due to soft clipping, set mapq high
+            #pipe to awk to get rid of ambiguous aligment (reverse forward orientation in GATK definition)
+            filter_task = "%s view -h -@ 6 -F 4 -F 256 -F 2048 -q 40 %s | awk -F'\t' '(!($2==83 && $9>0)) && (!($2==163 && $9<0)) && (!($2==99 && $9<0)) && (!($2==147 && $9>0)) {print $0}' | %s view -@ 6 -hbS - > %s\n\n" % (samtools, dedupbam, samtools, filterbam)
+            fo.write(filter_task)
+
+            extract_task = '%s --ignore 10 --ignore_r2 15 --ignore_3prime 3 --ignore_3prime_r2 3 --CX -p --samtools_path %s --gzip --parallel 2 -o %s --bedGraph %s\n\n' % (bismarkextractor, samtoolsdir, sample_sub_workdir, filterbam)
+            fo.write(extract_task)
+
+    write_sub_script_qsub(DNAalign_scriptdir, 'DNAalign', DNAalign_workdir, logdir)
+
+
+def write_sub_script_coverage(DNASampleIDs, scriptdir, workdir, logdir):
+    cov_scriptdir = scriptdir + '/4_cov'
+    cov_workdir = workdir + '/4_cov'
+    helper.mkdir(cov_scriptdir)
+    helper.mkdir(cov_workdir)
+
+    for sampleid in DNASampleIDs:
+
+        samplescript = cov_scriptdir + '/' + sampleid + '.cov.task.pbs'
+        samplescript2 = cov_scriptdir + '/' + sampleid + '.readcount.task.pbs'
+        cov_sub_workdir = cov_workdir + '/' + sampleid
+        helper.mkdir(cov_sub_workdir)
+
+        with open(samplescript, 'w+') as fo:
+            pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (12)
+            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (3)
+            pbsuser = '#PBS -M %s\n' % (noticeaccount)
+            pbsjobname = '#PBS -N cov_%s\n' % (sampleid)
+            pbscredit = '#PBS -A %s\n\n' % (creditaccount)
+            pbsaddsource = 'source %s\n\n' % (addsourcetool)
+            fo.write('#!/bin/bash -l\n\n')
+            fo.write(pbsjobtime)
+            fo.write(pbsnode)
+            fo.write('#PBS -l pmem=5gb\n')
+            fo.write(pbsuser)
+            fo.write('#PBS -m abe\n')
+            fo.write(pbsjobname)
+            fo.write(pbscredit)
+            fo.write(pbsaddsource)
+
+            #each step based on previous step; maybe wrap previous step into this step
+            DNAalign_workdir = workdir + '/3_Bismark'
+            DNAalign_sub_workdir = DNAalign_workdir + '/' + sampleid
+            filteredbam = '%s/%s.merge.deduplicated.filtered.FR.bam' % (DNAalign_sub_workdir, sampleid)
+            ###bismark bam not in standard GATK bam format, need to add readgroup and sort before feeding into GATK programs
+            infilteredbam = '%s/%s.merge.deduplicated.filtered.FR.sorted.bam' % (DNAalign_sub_workdir, sampleid)
+            addrgsort_task = '%s -jar %s AddOrReplaceReadGroups INPUT=%s OUTPUT=%s RGLB=BSseq RGPL=Illumina RGSM=%s RGPU=GC SORT_ORDER=coordinate CREATE_INDEX=true\n\n' % (java, picard, filteredbam, infilteredbam, sampleid)
+            fo.write(addrgsort_task)
+
+            #count coverage in terms of fragments (overlapped pair-end reads)
+            fragcov = '%s.merge.deduplicated.filtered.FR.sorted.fragcov' % (sampleid)
+            fragcov_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T DepthOfCoverage -R %s -o %s/%s -I %s -L %s --countType COUNT_FRAGMENTS -drf DuplicateRead\n\n' % (java, logdir, gatk, reference, cov_sub_workdir, fragcov, infilteredbam, target)
+            fo.write(fragcov_task)
+            ontarbam = '%s/%s.merge.deduplicated.filtered.FR.sorted.ontar.bam' % (DNAalign_sub_workdir, sampleid)
+            targetbam_task = '%s view -hbS %s -L %s > %s\n\n' % (samtools, infilteredbam, target, ontarbam)
+            fo.write(targetbam_task)
+            allinsert = '%s.merge.deduplicated.filtered.FR.sorted.insert_size_metrics' % (sampleid)
+            allinsertS_task = '%s -Xmx4G -jar %s CollectInsertSizeMetrics I=%s O=%s/%s.txt H=%s/%s.pdf DEVIATIONS=50 INCLUDE_DUPLICATES=false\n\n' % (java, picard, infilteredbam, cov_sub_workdir, allinsert, cov_sub_workdir, allinsert)
+            fo.write(allinsertS_task)
+            ontarinsert = '%s.merge.deduplicated.filtered.FR.sorted.ontar.insert_size_metrics' % (sampleid)
+            ontarinsertS_task = '%s -Xmx4G -jar %s CollectInsertSizeMetrics I=%s O=%s/%s.txt H=%s/%s.pdf DEVIATIONS=50 INCLUDE_DUPLICATES=false\n\n' % (java, picard, ontarbam, cov_sub_workdir, ontarinsert, cov_sub_workdir, ontarinsert)
+            fo.write(ontarinsertS_task)
+
+        ##output of CountReads is problematic, needs to be solved
+        with open(samplescript2, 'w+') as fo2:
+            pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (12)
+            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (3)
+            pbsuser = '#PBS -M %s\n' % (noticeaccount)
+            pbsjobname = '#PBS -N readcount_%s\n' % (sampleid)
+            pbscredit = '#PBS -A %s\n\n' % (creditaccount)
+            pbsaddsource = 'source %s\n\n' % (addsourcetool)
+            fo2.write('#!/bin/bash -l\n\n')
+            fo2.write(pbsjobtime)
+            fo2.write(pbsnode)
+            fo2.write('#PBS -l pmem=5gb\n')
+            fo2.write(pbsuser)
+            fo2.write('#PBS -m abe\n')
+            fo2.write(pbsjobname)
+            fo2.write(pbscredit)
+            fo2.write(pbsaddsource)
+
+            #count total filtered reads
+            #CountReads write to stdout
+            #readcountall = '%s.sorted.merged.md.filtered.readcountall.out' % (sampleid)
+            readcountall_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam)
+            fo2.write(readcountall_task)
+            #count on target reads
+            #readcountontar = '%s.sorted.merged.md.filtered.readcountOnTar.out' % (sampleid)
+            readcountontar_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -L %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam, target)
+            fo2.write(readcountontar_task)
+            #count flanked on target reads
+            #readcountontar50 = '%s.sorted.merged.md.filtered.readcountOnTar.50bpflank.out' % (sampleid)
+            readcountontar50_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -L %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam, targetflank50)
+            fo2.write(readcountontar50_task)
+
+            #readcountontar100 = '%s.sorted.merged.md.filtered.readcountOnTar.100bpflank.out' % (sampleid)
+            readcountontar100_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -L %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam, targetflank100)
+            fo2.write(readcountontar100_task)
+
+    write_sub_script_qsub(cov_scriptdir, 'cov', cov_workdir, logdir)
+    write_sub_script_qsub(cov_scriptdir, 'readcount', cov_workdir, cov_workdir)
+
+
+def write_scripts():
+    mytime = time.strftime('%y_%m_%d_%H_%M')
+    scriptdir = workdir + '/scripts_' + mytime
+    helper.mkdir(scriptdir)
+    logdir = scriptdir + '/log'
+    helper.mkdir(logdir)
+
+    write_sub_script_fastqc(DNASampleIDs, fastqdir, scriptdir, workdir, logdir)
+    write_sub_script_DNAtrimming(DNASampleIDs, fastqdir, scriptdir, workdir, logdir, DNAtrimoption)
+    write_sub_script_DNAalign(DNASampleIDs, fastqdir, scriptdir, workdir, logdir)
+    write_sub_script_coverage(DNASampleIDs, scriptdir, workdir, logdir)
+
+
+if __name__ == '__main__':
+    write_scripts()
