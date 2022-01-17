c2f5cbc113df8b24a5b023e22ba6012857396d78
chehuiw <chehuiw@hotmail.com>
2022-01-17 16:41:50 +0100

    import

--- tmp/downstream_ana.R
+++ tmp/downstream_ana.R
@@ -1,16 +1,22 @@
 ####downstream analysis using seqmonk output
 
 library(data.table)
+library(reshape2)
+
+args <- commandArgs(TRUE)
+indexfh <- args[1]
+frlist <- args[2]
+outfh <- args[3]
 
 LoadProbeIndex <- function(indexfh) {
     idx <- fread(indexfh, header=TRUE, sep="\t", data.table=FALSE)
     return(idx)
 }
 
-GetAvgCount <- function(frlist, indexfh) {
-    samlist <- fread(samples.file, header=FALSE, sep="\t", colClasses=c("character", "character"), data.table=FALSE)
+GetAvgMatrix <- function(frlist, indexfh, outfh) {
+    samlist <- fread(frlist, header=FALSE, sep="\t", colClasses=c("character", "character"), data.table=FALSE)
     idx <- LoadProbeIndex(indexfh)
-    
+    dtmat <- data.frame()
     for (i in 1:nrow(samlist)) {
          print(paste("reading in", samlist[i,1]))
          fwdfh <- fread(samlist[i,1], header=TRUE, sep="\t", data.table=FALSE)
@@ -31,10 +37,23 @@ GetAvgCount <- function(frlist, indexfh) {
 	 colnames(idxrevfh_long) <- c("Index","Sample","Revval")
 	 
 	 dt_long <- merge(idxfwdfh_long, idxrevfh_long, by=c("Index","Sample"))
-	 dt_long$Total <- dt_long$Fwdval+dt$Revval
+	 dt_long$Total <- dt_long$Fwdval+dt_long$Revval
 	 dt_long$Avgmc <- dt_long$Fwdval/dt_long$Total
-	 
 	 #dtrt <- dcast(melt(dt_long, id.vars=c("Index","Sample")), Index~Sample+variable)
 	 dt <- dcast(dt_long, Index ~ Sample, value.var="Avgmc")
+	 dt <- dt[order(dt$Index),]
+	 dx <- t(dt[,-1])
+	 #colnames(dx) <- paste0("P",order(dt$Index))
+	 dtmat <- rbind(dtmat, dx)
     }
+    #write.table(dtmat, outfh, sep="\t", row.names=TRUE, quote=FALSE)
+    return(dtmat)
+}
+
+FilterSample <- function() {
+    #todo
+}
+
+FetchSampleInfo <- function() {
+    
 }
