a5c4c96e88ec7b4b5633071e2cd133a9c6040998
chehuiw <chehuiw@hotmail.com>
2022-10-11 14:33:55 +0200

    bioinfo proc

---
+++ processing/getinsertsize.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import time
+import pysam
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import scipy.stats as st
+from collections import defaultdict
+
+def find_read_pair(inbam, mapregion=None):
+    #initialize two-level dict
+    matedict = defaultdict(lambda: defaultdict(None))
+    for read in inbam.fetch(region=mapregion):
+        if not read.is_proper_pair or read.is_secondary or read.is_duplicate or read.is_supplementary or read.is_unmapped or read.mapping_quality < 10:
+            continue
+        matename = read.query_name
+        if matename not in matedict:
+            if read.is_read1:
+                matedict[matename][0] = read
+            else:
+                matedict[matename][1] = read
+        else:
+            if read.is_read1:
+                yield read, matedict[matename][1]
+            else:
+                yield matedict[matename][0], read
+            del matedict[matename]
+
+
+def get_insert_size(inbam):
+    fsizedict = defaultdict()
+    outfh = f'{inbam}.finsert.tsv'
+    outfig = f'{inbam}.finsert.pdf'
+    farr = []
+
+    with open(outfh, 'w') as fo:
+        with pysam.AlignmentFile(inbam, mode='rb') as fh:
+            buffchunk = ""
+            buffl = 0
+            for read1, read2 in find_read_pair(fh):
+                finsert = 0
+                readname = read1.query_name
+                read1ref = read1.reference_name
+                read1start = read1.reference_start
+                read2start = read1.next_reference_start
+
+                if read1.is_reverse:
+                    read1cigarA = read1.get_cigar_stats()[0]
+                    read1mbp = read1cigarA[0]+read1cigarA[2]
+                    finsert = read1start + read1mbp - read2start
+                else:
+                    read2cigarA = read2.get_cigar_stats()[0]
+                    read2mbp = read2cigarA[0]+read2cigarA[2]
+                    finsert = read2start + read2mbp - read1start
+                #pfline = f'{readname}\t{read1ref}\t{read1start}\t{read2start}\t{finsert}\n'
+                #fo.write(pfline)
+                farr.append(finsert)
+                if finsert in fsizedict:
+                    fsizedict[finsert] += 1
+                else:
+                    fsizedict[finsert] = 1
+
+        for record, count in sorted(fsizedict.items(), key=lambda item: item[0]):
+            pfline = f'{record}\t{count}\n'
+            fo.write(pfline)
+
+        figtitle = f'{os.path.basename(inbam)}.Histrogram'
+        #plt.hist(farr, density=True, bins=500, label="InsertSize")
+        #mn, mx = plt.xlim()
+        #kde_xs = np.linspace(mn,mx,300)
+        #kde = st.gaussian_kde(farr)
+        #plt.plot(kde_xs, kde.pdf(kde_xs), label="PDF")
+
+        num_bins = min(max(farr),500)
+        fig, ax = plt.subplots()
+        sns.histplot(farr, binwidth=1, ax=ax, color="lightblue")
+        ax2 = ax.twinx()
+        bin_width = (max(farr) - min(farr)) / num_bins
+        hist_area = len(farr) * bin_width
+        ax2.set_ylim(ymax=ax.get_ylim()[1] / hist_area)
+        sns.kdeplot(farr,ax=ax2)
+        plt.xlabel("InsertSize")
+        plt.setp(ax.patches, linewidth=0)
+        plt.title(figtitle)
+        plt.tight_layout()
+        plt.savefig(outfig)
+        plt.close(outfig)
+
+
+if __name__ == '__main__':
+    get_insert_size(sys.argv[1])
--- processing/process.alignment.py
+++ processing/process.alignment.py
@@ -6,7 +6,7 @@ import time
 import helper
 
 (fastqdir, workdir, runscriptdir) = helper.get_dir_config()
-(samtools, samtoolsdir, fastqc, trimtool, bismark, bismarkdedup, bismarkextractor, bismarkcytosine, bowtiedir, java, gatk, picard, addsourcetool, python, bismarkreference, reference, target, targetflank50, targetflank100) = helper.get_run_config()
+(samtools, samtoolsdir, fastqc, trimtool, bismark, bismarkdedup, bismarkextractor, bismarkcytosine, java, gatk, picard, addsourcetool, python, bismarkreference, reference, target, targetflank50, targetflank100) = helper.get_run_config()
 DNASampleIDs = helper.get_DNA_sample()
 (DNAtrimoption) = helper.get_option()
 (noticeaccount, creditaccount) = helper.get_userinfo()
@@ -108,7 +108,7 @@ def write_sub_script_DNAtrimming(DNASampleIDs, fastqdir, scriptdir, workdir, log
     write_sub_script_qsub(trim_scriptdir, 'DNAtrim', trim_workdir, logdir)
 
 
-def write_sub_script_DNAalign(DNASampleIDs, fastqdir, scriptdir, workdir, logdir):
+def write_sub_script_DNAalign(DNASampleIDs, scriptdir, workdir, logdir):
     trim_workdir = workdir + '/2_DNAtrim'
     DNAalign_scriptdir = scriptdir + '/3_Bismarkalign'
     DNAalign_workdir = workdir + '/3_Bismark'
@@ -148,7 +148,7 @@ def write_sub_script_DNAalign(DNASampleIDs, fastqdir, scriptdir, workdir, logdir
             trimFQR2 = trimFQR1.replace("R1_","R2_").replace("val_1","val_2")
 
             #directional library
-            DNAalign_task = '%s -N 1 -L 20 --multicore 6 --dovetail --gzip --path_to_bowtie %s --samtools_path %s --genome %s -1 %s -2 %s --output_dir %s --temp_dir %s\n\n' % (bismark, bowtiedir, samtoolsdir, bismarkreference, trimFQR1, trimFQR2, sample_sub_workdir, sample_sub_workdir)
+            DNAalign_task = '%s -N 1 -L 20 --multicore 6 --dovetail --gzip --samtools_path %s --genome %s -1 %s -2 %s --output_dir %s --temp_dir %s\n\n' % (bismark, samtoolsdir, bismarkreference, trimFQR1, trimFQR2, sample_sub_workdir, sample_sub_workdir)
             fo.write(DNAalign_task)
 
             alignbams = trimFQR1.replace(trim_workdir, sample_sub_workdir).replace(".fq.gz","_bismark_bt2_pe.bam").replace(",", " ")
@@ -165,7 +165,7 @@ def write_sub_script_DNAalign(DNASampleIDs, fastqdir, scriptdir, workdir, logdir
             #filter_task = "%s view -bS -@ 4 -F 4 -F 256 -F 2048 -q 40 %s > %s\n\n" % (samtools, dedupbam, filterbam)
             #fo.write(filter_task)
 
-            extract_task = '%s -p --gzip --parallel 4 --samtools_path %s -o %s --bedGraph %s\n\n' % (bismarkextractor, samtoolsdir, sample_sub_workdir, filterbam)
+            extract_task = '%s -p --gzip --parallel 4 --samtools_path %s -o %s --bedGraph %s\n\n' % (bismarkextractor, samtoolsdir, sample_sub_workdir, dedupbam)
             fo.write(extract_task)
 
     write_sub_script_qsub(DNAalign_scriptdir, 'DNAalign', DNAalign_workdir, logdir)
@@ -186,7 +186,7 @@ def write_sub_script_coverage(DNASampleIDs, scriptdir, workdir, logdir):
 
         with open(samplescript, 'w+') as fo:
             pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (12)
-            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (3)
+            pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (4)
             pbsuser = '#PBS -M %s\n' % (noticeaccount)
             pbsjobname = '#PBS -N cov_%s\n' % (sampleid)
             pbscredit = '#PBS -A %s\n\n' % (creditaccount)
@@ -201,35 +201,36 @@ def write_sub_script_coverage(DNASampleIDs, scriptdir, workdir, logdir):
             fo.write(pbscredit)
             fo.write(pbsaddsource)
 
-            #each step based on previous step; maybe wrap previous step into this step
             DNAalign_workdir = workdir + '/3_Bismark'
             DNAalign_sub_workdir = DNAalign_workdir + '/' + sampleid
-            filteredbam = '%s/%s.merge.deduplicated.filtered.bam' % (DNAalign_sub_workdir, sampleid)
+            for bamfile in os.listdir(DNAalign_sub_workdir):
+                if bamfile.endswith('deduplicated.bam'):
+                    filteredbam = bamfile
+
             ###bismark bam not in standard GATK bam format, need to add readgroup and sort before feeding into GATK programs
-            infilteredbam = '%s/%s.merge.deduplicated.filtered.sorted.bam' % (DNAalign_sub_workdir, sampleid)
+            infilteredbam = '%s/%s.merge.deduplicated.sorted.bam' % (DNAalign_sub_workdir, sampleid)
             addrgsort_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s AddOrReplaceReadGroups INPUT=%s OUTPUT=%s RGLB=BSseq RGPL=Illumina RGSM=%s RGPU=GC SORT_ORDER=coordinate CREATE_INDEX=true\n\n' % (java, logdir, picard, filteredbam, infilteredbam, sampleid)
             fo.write(addrgsort_task)
             indexbam_task = '%s index %s\n\n' % (samtools, infilteredbam)
             fo.write(indexbam_task)
 
-            calfinsert_task = '%s %s/upstream/getinsertsize.py %s\n\n' % (python, runscriptdir, infilteredbam)
+            calfinsert_task = '%s %s/processing/getinsertsize.py %s\n\n' % (python, runscriptdir, infilteredbam)
             fo.write(calfinsert_task)
 
             #count coverage in terms of fragments (overlapped pair-end reads)
-            fragcov = '%s.merge.deduplicated.filtered.sorted.fragcov' % (sampleid)
+            fragcov = '%s.merge.deduplicated.sorted.fragcov' % (sampleid)
             fragcov_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T DepthOfCoverage -R %s -o %s/%s -I %s -L %s --countType COUNT_FRAGMENTS -drf DuplicateRead\n\n' % (java, logdir, gatk, reference, cov_sub_workdir, fragcov, infilteredbam, target)
             fo.write(fragcov_task)
-            ontarbam = '%s/%s.merge.deduplicated.filtered.sorted.ontar.bam' % (DNAalign_sub_workdir, sampleid)
+            ontarbam = '%s/%s.merge.deduplicated.sorted.ontar.bam' % (DNAalign_sub_workdir, sampleid)
             targetbam_task = '%s view -hbS %s -L %s > %s\n\n' % (samtools, infilteredbam, target, ontarbam)
             fo.write(targetbam_task)
-            allinsert = '%s.merge.deduplicated.filtered.sorted.insert_size_metrics' % (sampleid)
+            allinsert = '%s.merge.deduplicated.sorted.insert_size_metrics' % (sampleid)
             allinsertS_task = '%s -Xmx4G -jar %s CollectInsertSizeMetrics I=%s O=%s/%s.txt H=%s/%s.pdf DEVIATIONS=50 INCLUDE_DUPLICATES=false\n\n' % (java, picard, infilteredbam, cov_sub_workdir, allinsert, cov_sub_workdir, allinsert)
             fo.write(allinsertS_task)
-            ontarinsert = '%s.merge.deduplicated.filtered.sorted.ontar.insert_size_metrics' % (sampleid)
+            ontarinsert = '%s.merge.deduplicated.sorted.ontar.insert_size_metrics' % (sampleid)
             ontarinsertS_task = '%s -Xmx4G -jar %s CollectInsertSizeMetrics I=%s O=%s/%s.txt H=%s/%s.pdf DEVIATIONS=50 INCLUDE_DUPLICATES=false\n\n' % (java, picard, ontarbam, cov_sub_workdir, ontarinsert, cov_sub_workdir, ontarinsert)
             fo.write(ontarinsertS_task)
 
-        ##output of CountReads is problematic, needs to be solved
         with open(samplescript2, 'w+') as fo2:
             pbsjobtime = '#PBS -l walltime=%s:00:00\n' % (12)
             pbsnode = '#PBS -l nodes=1:ppn=%s\n' % (3)
@@ -249,19 +250,18 @@ def write_sub_script_coverage(DNASampleIDs, scriptdir, workdir, logdir):
 
             #count total filtered reads
             #CountReads write to stdout
-            #readcountall = '%s.sorted.merged.md.filtered.readcountall.out' % (sampleid)
+
             readcountall_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam)
             fo2.write(readcountall_task)
+
             #count on target reads
-            #readcountontar = '%s.sorted.merged.md.filtered.readcountOnTar.out' % (sampleid)
             readcountontar_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -L %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam, target)
             fo2.write(readcountontar_task)
+
             #count flanked on target reads
-            #readcountontar50 = '%s.sorted.merged.md.filtered.readcountOnTar.50bpflank.out' % (sampleid)
             readcountontar50_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -L %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam, targetflank50)
             fo2.write(readcountontar50_task)
 
-            #readcountontar100 = '%s.sorted.merged.md.filtered.readcountOnTar.100bpflank.out' % (sampleid)
             readcountontar100_task = '%s -Xmx4G -XX:-UsePerfData -XX:-UseParallelGC -Djava.io.tmpdir=%s -jar %s -T CountReads -R %s -I %s -L %s -drf DuplicateRead\n' % (java, logdir, gatk, reference, infilteredbam, targetflank100)
             fo2.write(readcountontar100_task)
 
@@ -278,7 +278,7 @@ def write_scripts():
 
     write_sub_script_fastqc(DNASampleIDs, fastqdir, scriptdir, workdir, logdir)
     write_sub_script_DNAtrimming(DNASampleIDs, fastqdir, scriptdir, workdir, logdir, DNAtrimoption)
-    write_sub_script_DNAalign(DNASampleIDs, fastqdir, scriptdir, workdir, logdir)
+    write_sub_script_DNAalign(DNASampleIDs, scriptdir, workdir, logdir)
     write_sub_script_coverage(DNASampleIDs, scriptdir, workdir, logdir)
 
 
